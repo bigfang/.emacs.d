7e6553c1fa39373d74971ceb1bf480e4460fdca0
bigfang <bitair@gmail.com>
2018-03-07 22:49:56 +0800

    更舒服的ibuffer

--- config/02.modes-conf.el
+++ config/02.modes-conf.el
@@ -80,6 +80,11 @@
          ("Lisp" (or (mode . lisp-mode)
                      ;; (mode . lisp-interaction-mode)
                      (mode . emacs-lisp-mode)))
+         ("Magit"  (name . "^\\*magit:"))
+         ("VC"  (or (name . "^\\*magit-")
+                    (name . "^\\*vc")
+                    (mode . diff-mode)
+                    (mode . vc-dir-mode)))
          ("Erc" (or (mode . erc-mode)
                     (mode . erc-list-mode)))
          ("Gnus" (or (mode . message-mode)
@@ -96,6 +101,28 @@
 (add-hook 'ibuffer-mode-hook
           (lambda ()
             (ibuffer-switch-to-saved-filter-groups "Default")))
+
+;; Use human readable Size column instead of original one
+(define-ibuffer-column size-h
+  (:name "Size" :inline t)
+  (cond
+   ((> (buffer-size) 1000000) (format "%7.1fM" (/ (buffer-size) 1000000.0)))
+   ((> (buffer-size) 100000) (format "%7.0fk" (/ (buffer-size) 1000.0)))
+   ((> (buffer-size) 1000) (format "%7.1fk" (/ (buffer-size) 1000.0)))
+   (t (format "%8d" (buffer-size)))))
+
+;; Modify the default ibuffer-formats
+(setq ibuffer-formats
+      '((mark modified read-only " "
+              (name 24 24 :left :elide)
+              " "
+              (size-h 9 -1 :right)
+              " "
+              (mode 16 16 :left :elide)
+              " " filename-and-process)
+        (mark " "
+              (name 16 -1)
+              " " filename)))
 (global-set-key (kbd "C-x b") 'ibuffer)
 
 ;; ido
