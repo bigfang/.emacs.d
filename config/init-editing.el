33950d7d32ad4e2f719222fa90ee6ebc0b95faaa
bigfang <bitair@gmail.com>
2018-03-17 21:40:32 +0800

    更聪明的去空白符

--- config/init-editing.el
+++ config/init-editing.el
@@ -4,7 +4,7 @@
 (setq-default fill-column 80)
 (setq-default indent-tabs-mode nil)
 (setq echo-keystrokes 0.1)              ; 按键连击间隔
-(setq backward-delete-char-untabify-method 'untabify) ; 删除键行为
+(setq backward-delete-char-untabify-method 'hungry) ; 删除键行为
 (setq select-enable-clipboard t)        ; 系统剪切板
 (setq save-interprogram-paste-before-kill t) ; 剪切板内容加入killing-ring
 (setq tab-always-indent 'complete)           ; tab健行为
@@ -26,11 +26,8 @@
 (use-package files
   :ensure nil
   :config
-  ;; final blank line
-  (setq require-final-newline t)
-
   ;; time-stamp
-  (add-hook 'write-file-hooks 'time-stamp)
+  (add-hook 'before-save-hook 'time-stamp)
   (setq time-stamp-format "%04y-%02m-%02d %02H:%02M:%02S")
 
   ;; Auto-save and Backups
@@ -45,20 +42,30 @@
         delete-old-versions t)
   (setq backup-directory-alist
         `((".*" . ,temporary-file-directory)))
-  )
 
+    ;; final blank line
+  (setq require-final-newline t))
+
+
+(defun whitespace-cleanup* ()
+  (let* ((modified-before-p (buffer-modified-p)))
+    (whitespace-cleanup)
+    (if (not modified-before-p)
+        (not-modified))))
+
+(defun whitespace-cleanup-on-save ()
+  (add-hook 'write-contents-functions #'whitespace-cleanup*))
 
 ;; prog-mode hooks
 (add-hook 'prog-mode-hook
           (lambda ()
-            (add-to-list 'write-file-functions 'delete-trailing-whitespace)
+            (whitespace-cleanup-on-save)
             ;; (linum-mode)
             (hs-minor-mode)
             (highlight-indentation-mode)
             (prettify-symbols-mode)
             (setq truncate-lines t)
-            (setq fill-column 120)
-            ))
+            (setq fill-column 120)))
 
 
 (use-package expand-region
