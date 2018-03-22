0980b0da9686c3de85ed27e1871c14b605d9a6f5
bigfang <bitair@gmail.com>
2018-03-22 22:40:48 +0800

    更友好的magit界面

--- config/init-git.el
+++ config/init-git.el
@@ -6,15 +6,32 @@
   :pin melpa-stable
   :bind ("C-c v" . magit-status)
   :config
+  (setq magit-display-buffer-function
+        (lambda (buffer)
+          (display-buffer
+           buffer
+           (cond ((and (derived-mode-p 'magit-mode)
+                       (eq (with-current-buffer buffer major-mode)
+                           'magit-status-mode))
+                  nil)
+                 ((memq (with-current-buffer buffer major-mode)
+                        '(magit-process-mode
+                          magit-revision-mode
+                          magit-diff-mode
+                          magit-stash-mode))
+                  nil)
+                 (t
+                  '(display-buffer-same-window))))))
+
   (setq magit-completing-read-function 'ivy-completing-read))
+
 
 (use-package git-gutter
   :ensure t
   :config
   (global-git-gutter-mode +1)
   :custom
-  (git-gutter:disabled-modes '(asm-mode hexl-mode image-mode))
-  )
+  (git-gutter:disabled-modes '(asm-mode hexl-mode image-mode)))
 
 
 (provide 'init-git)
