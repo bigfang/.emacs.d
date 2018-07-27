092a4e5761abf669a1bc35c46ba5c7ae6b6dcca8
bigfang <bitair@gmail.com>
2018-07-27 10:03:37 +0800

    用highlight-indent-guides显示缩进线

--- config/init-elpa.el
+++ config/init-elpa.el
@@ -60,6 +60,21 @@
     ("b" dumb-jump-back "Back")))
 
 
+(use-package highlight-indent-guides
+  :ensure t
+  :hook (prog-mode . highlight-indent-guides-mode)
+  :config
+  (setq highlight-indent-guides-method 'character)
+  (setq highlight-indent-guides-responsive 'top
+        highlight-indent-guides-delay 0.3)
+
+  (setq highlight-indent-guides-auto-enabled nil)
+  (set-face-background 'highlight-indent-guides-odd-face "darkgray")
+  (set-face-background 'highlight-indent-guides-even-face "dimgray")
+  (set-face-foreground 'highlight-indent-guides-character-face "dimgray")
+  (set-face-foreground 'highlight-indent-guides-top-character-face "wheat4"))
+
+
 (use-package highlight-symbol
   :ensure t
   :chords ("[[" . highlight-symbol-at-point)
