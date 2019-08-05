96103c38dc48a47a8c1a2d1758a358f2ccfe1b2a
bigfang <bitair@gmail.com>
2019-08-05 23:34:47 +0800

    fix move-text

--- hydras/hydra-builtin.el
+++ hydras/hydra-builtin.el
@@ -38,23 +38,6 @@
   ("q" nil :color blue))
 
 
-(defhydra hydra:move-text (:color red :hint nil)
-  "Move text"
-  ("<up>" move-text-up "up")
-  ("<down>" move-text-down "down")
-  ("q" nil :color blue))
-
-(global-set-key (kbd "<C-S-up>") '(lambda ()
-                                    (interactive)
-                                    (move-text-up)
-                                    (hydra:move-text/body)))
-
-(global-set-key (kbd "<C-S-down>") '(lambda ()
-                                      (interactive)
-                                      (move-text-down)
-                                      (hydra:move-text/body)))
-
-
 (defhydra hydra:mark (:color pink :exit t :hint nil :columns 3)
   "Marks"
   ("m" counsel-bookmark "bookmark")
